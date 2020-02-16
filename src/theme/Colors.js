782c4345d6ab3d7ce2adc7388ac97517e5d17bfe
Ricardo Saraiva <ricardobboy_@hotmail.com>
2020-02-16 16:58:33 -0300

    Create theme for project

---
+++ src/theme/Colors.js
@@ -0,0 +1,14 @@
+export default {
+  cyan: {
+    _100: '#d5fef6',
+    _300: '#80deea',
+    _500: '#00bcd4',
+    _900: '#006064',
+  },
+  yellow: {
+    _500: '#ffab00',
+  },
+  foreground: {
+    _100: '#ffffff',
+  },
+};
---
+++ src/theme/index.js
@@ -0,0 +1,5 @@
+import themeRadius from './Radius';
+import themeColor from './Colors';
+
+export const Radius = themeRadius;
+export const Colors = themeColor;
